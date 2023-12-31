df6a6d6aeb960eab177d43e4818ef2e4754730b6
Abdessittir <harkati.web.dev@gmail.com>
2023-12-31 14:05:32 +0100

    Add queen and bishop test for isKingInCheck function

--- tests/client/gamelogic/king.test.ts
+++ tests/client/gamelogic/king.test.ts
@@ -5,7 +5,7 @@ import { isKingInCheck } from '../../../gameLogic/kingCalculation';
 describe('King tests/ isKingInCheck', () => {
   const whiteKing = 'KGE1';
   const blackKing = 'KGE8';
-  /*
+
   test('test against pawns', () => {
     // Arrange
     // test white king
@@ -38,7 +38,6 @@ describe('King tests/ isKingInCheck', () => {
     expect(inCheck3).toBe(true);
     expect(inCheck4).toBe(true);
   });
-  */
 
   describe('test white king against queen and rook', () => {
     test('checkVerticalLineToTop', () => {
@@ -198,15 +197,157 @@ describe('King tests/ isKingInCheck', () => {
   });
 
   describe('test white king against queen and bishop', () => {
-    test('checkLineToRightTop', () => { });
-    test('checkLineToLeftBottom', () => { });
-    test('checkLineToLeftTop', () => { });
-    test('checkLineToRightBottom', () => { });
+    test('checkLineToRightTop', () => {
+      // Arrange
+      const pieces1 = new Map<string, PieceType>([...initialPieces]);
+      pieces1.set(whiteKing, { ...pieces1.get(whiteKing) as PieceType, y: 2 });
+      pieces1.set('BF8', { ...pieces1.get('BF8') as PieceType, x: 7, y: 5 });
+
+      const pieces2 = new Map<string, PieceType>([...initialPieces]);
+      pieces2.set(whiteKing, { ...pieces2.get(whiteKing) as PieceType, y: 2 });
+      pieces2.set('QD8', { ...pieces2.get('QD8') as PieceType, x: 7, y: 5 });
+
+      // Act
+      const inCheck1 = isKingInCheck(pieces1.get(whiteKing) as PieceType, pieces1);
+      const inCheck2 = isKingInCheck(pieces2.get(whiteKing) as PieceType, pieces2);
+
+      // Assert
+      expect(inCheck1).toBe(true);
+      expect(inCheck2).toBe(true);
+    });
+
+    test('checkLineToLeftBottom', () => {
+      // Arrange
+      const pieces1 = new Map<string, PieceType>([...initialPieces]);
+      pieces1.set(whiteKing, { ...pieces1.get(whiteKing) as PieceType, x: 6, y: 4 });
+      pieces1.set('BF8', { ...pieces1.get('BF8') as PieceType, x: 4, y: 2 });
+
+      const pieces2 = new Map<string, PieceType>([...initialPieces]);
+      pieces2.set(whiteKing, { ...pieces2.get(whiteKing) as PieceType, x: 6, y: 4 });
+      pieces2.set('QD8', { ...pieces2.get('QD8') as PieceType, x: 4, y: 2 });
+
+      // Act
+      const inCheck1 = isKingInCheck(pieces1.get(whiteKing) as PieceType, pieces1);
+      const inCheck2 = isKingInCheck(pieces2.get(whiteKing) as PieceType, pieces2);
+
+      // Assert
+      expect(inCheck1).toBe(true);
+      expect(inCheck2).toBe(true);
+    });
+    test('checkLineToLeftTop', () => {
+      // Arrange
+      const pieces1 = new Map<string, PieceType>([...initialPieces]);
+      pieces1.set(whiteKing, { ...pieces1.get(whiteKing) as PieceType, y: 2 });
+      pieces1.set('BF8', { ...pieces1.get('BF8') as PieceType, x: 1, y: 5 });
+
+      const pieces2 = new Map<string, PieceType>([...initialPieces]);
+      pieces2.set(whiteKing, { ...pieces2.get(whiteKing) as PieceType, y: 2 });
+      pieces2.set('QD8', { ...pieces2.get('QD8') as PieceType, x: 1, y: 5 });
+
+      // Act
+      const inCheck1 = isKingInCheck(pieces1.get(whiteKing) as PieceType, pieces1);
+      const inCheck2 = isKingInCheck(pieces2.get(whiteKing) as PieceType, pieces2);
+
+      // Assert
+      expect(inCheck1).toBe(true);
+      expect(inCheck2).toBe(true);
+    });
+
+    test('checkLineToRightBottom', () => {
+      // Arrange
+      const pieces1 = new Map<string, PieceType>([...initialPieces]);
+      pieces1.set(whiteKing, { ...pieces1.get(whiteKing) as PieceType, x: 2, y: 4 });
+      pieces1.set('BF8', { ...pieces1.get('BF8') as PieceType, x: 4, y: 2 });
+
+      const pieces2 = new Map<string, PieceType>([...initialPieces]);
+      pieces2.set(whiteKing, { ...pieces2.get(whiteKing) as PieceType, x: 2, y: 4 });
+      pieces2.set('QD8', { ...pieces2.get('QD8') as PieceType, x: 4, y: 2 });
+
+      // Act
+      const inCheck1 = isKingInCheck(pieces1.get(whiteKing) as PieceType, pieces1);
+      const inCheck2 = isKingInCheck(pieces2.get(whiteKing) as PieceType, pieces2);
+
+      // Assert
+      expect(inCheck1).toBe(true);
+      expect(inCheck2).toBe(true);
+    });
   });
+
   describe('test black king against queen and bishop', () => {
-    test('checkLineToRightTop', () => { });
-    test('checkLineToLeftBottom', () => { });
-    test('checkLineToLeftTop', () => { });
-    test('checkLineToRightBottom', () => { });
+    test('checkLineToRightTop', () => {
+      // Arrange
+      const pieces1 = new Map<string, PieceType>([...initialPieces]);
+      pieces1.set(blackKing, { ...pieces1.get(blackKing) as PieceType, x: 2, y: 3 });
+      pieces1.set('BF1', { ...pieces1.get('BF1') as PieceType, x: 4, y: 5 });
+
+      const pieces2 = new Map<string, PieceType>([...initialPieces]);
+      pieces2.set(blackKing, { ...pieces2.get(blackKing) as PieceType, x: 2, y: 3 });
+      pieces2.set('QD1', { ...pieces2.get('QD1') as PieceType, x: 4, y: 5 });
+
+      // Act
+      const inCheck1 = isKingInCheck(pieces1.get(blackKing) as PieceType, pieces1);
+      const inCheck2 = isKingInCheck(pieces2.get(blackKing) as PieceType, pieces2);
+
+      // Assert
+      expect(inCheck1).toBe(true);
+      expect(inCheck2).toBe(true);
+    });
+
+    test('checkLineToLeftBottom', () => {
+      // Arrange
+      const pieces1 = new Map<string, PieceType>([...initialPieces]);
+      pieces1.set(blackKing, { ...pieces1.get(blackKing) as PieceType, y: 5 });
+      pieces1.set('BF1', { ...pieces1.get('BF1') as PieceType, x: 2, y: 3 });
+
+      const pieces2 = new Map<string, PieceType>([...initialPieces]);
+      pieces2.set(blackKing, { ...pieces2.get(blackKing) as PieceType, y: 5 });
+      pieces2.set('QD1', { ...pieces2.get('QD1') as PieceType, x: 2, y: 3 });
+
+      // Act
+      const inCheck1 = isKingInCheck(pieces1.get(blackKing) as PieceType, pieces1);
+      const inCheck2 = isKingInCheck(pieces2.get(blackKing) as PieceType, pieces2);
+
+      // Assert
+      expect(inCheck1).toBe(true);
+      expect(inCheck2).toBe(true);
+    });
+
+    test('checkLineToLeftTop', () => {
+      // Arrange
+      const pieces1 = new Map<string, PieceType>([...initialPieces]);
+      pieces1.set(blackKing, { ...pieces1.get(blackKing) as PieceType, y: 3 });
+      pieces1.set('BF1', { ...pieces1.get('BF1') as PieceType, x: 2, y: 5 });
+
+      const pieces2 = new Map<string, PieceType>([...initialPieces]);
+      pieces2.set(blackKing, { ...pieces2.get(blackKing) as PieceType, y: 3 });
+      pieces2.set('QD1', { ...pieces2.get('QD1') as PieceType, x: 2, y: 5 });
+
+      // Act
+      const inCheck1 = isKingInCheck(pieces1.get(blackKing) as PieceType, pieces1);
+      const inCheck2 = isKingInCheck(pieces2.get(blackKing) as PieceType, pieces2);
+
+      // Assert
+      expect(inCheck1).toBe(true);
+      expect(inCheck2).toBe(true);
+    });
+
+    test('checkLineToRightBottom', () => {
+      // Arrange
+      const pieces1 = new Map<string, PieceType>([...initialPieces]);
+      pieces1.set(blackKing, { ...pieces1.get(blackKing) as PieceType, x: 2, y: 5 });
+      pieces1.set('BF1', { ...pieces1.get('BF1') as PieceType, x: 5, y: 2 });
+
+      const pieces2 = new Map<string, PieceType>([...initialPieces]);
+      pieces2.set(blackKing, { ...pieces2.get(blackKing) as PieceType, x: 2, y: 5 });
+      pieces2.set('QD1', { ...pieces2.get('QD1') as PieceType, x: 5, y: 2 });
+
+      // Act
+      const inCheck1 = isKingInCheck(pieces1.get(blackKing) as PieceType, pieces1);
+      const inCheck2 = isKingInCheck(pieces2.get(blackKing) as PieceType, pieces2);
+
+      // Assert
+      expect(inCheck1).toBe(true);
+      expect(inCheck2).toBe(true);
+    });
   });
 });
